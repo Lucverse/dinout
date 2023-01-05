33a8556b574249b38a24378668519b7613a19d45
Meshabaz <shabazameen1818@gmail.com>
2023-01-05 12:17:31 +0530

    make check

--- dineout/src/Components/Checkout/Checkout.jsx
+++ dineout/src/Components/Checkout/Checkout.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Checkout.css";
 
 const Checkout = () => {
+  const [tableStatus, setTableStatus] = useState(false);
+  setTimeout(() => {
+    setTableStatus(true);
+  }, 2000);
   return (
     <div className="container">
       <div className="main">
@@ -20,7 +24,7 @@ const Checkout = () => {
               <h5>Phone no.:</h5>
             </div>
             <div className="right">
-              <h5>The guest name ..</h5>
+              <h5>The guest name </h5>
               <h5>1234567890</h5>
             </div>
           </div>
@@ -33,16 +37,20 @@ const Checkout = () => {
           </div>
           <div className="lower">
             <div className="left">
-              <h5>
-                Table Status - <span className="tableStatus">checking</span>
-              </h5>
+              <h5>Table Status -</h5>
               <h5>Date & Time</h5>
               <h5>Guests</h5>
               <h5>Name</h5>
               <h5>ID</h5>
             </div>
             <div className="right">
-              <h5>Checking table status</h5>
+              <h5>
+                <span className="tableStatus">
+                  {tableStatus
+                    ? "Your reservation is confirmed! Happy Dining"
+                    : "Loading..."}
+                </span>
+              </h5>
               <h5>.......</h5>
               <h5>...........</h5>
               <h5>........</h5>
