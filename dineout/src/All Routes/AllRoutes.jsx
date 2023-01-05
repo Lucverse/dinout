8c690f8daf43774b3df0fb3d2b3cbff2d5726686
Meshabaz <shabazameen1818@gmail.com>
2023-01-05 22:10:09 +0530

    dine-out

--- dineout/src/All Routes/AllRoutes.jsx
+++ dineout/src/All Routes/AllRoutes.jsx
@@ -1,14 +1,18 @@
-import {Routes, Route} from "react-router-dom"
-import Home from "../Components/Home/home"
+import { Routes, Route } from "react-router-dom";
+import Checkout from "../Components/Checkout/Checkout";
+import Home from "../Components/Home/home";
+import Product from "../Components/ProductPages/Product";
 
 function AllRoutes() {
-    return (
-        <div>
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>  
-        </div>
-    )
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Product />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default AllRoutes;
--- dineout/src/Components/ProductPages/Products/ProductBox.jsx
+++ dineout/src/Components/ProductPages/Products/ProductBox.jsx
@@ -1,85 +1,83 @@
-import React, { useEffect, useState } from 'react';
-import "./productBox.css"
-import ProductAction from '../StoreContent/ProductAction';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import "./productBox.css";
+import ProductAction from "../StoreContent/ProductAction";
+import { useDispatch, useSelector } from "react-redux";
 // import store from '../../../Store';
-import DivProduct from './DivProduct';
-import { Link } from 'react-router-dom';
+import DivProduct from "./DivProduct";
+import { Link } from "react-router-dom";
 // import { Select } from '@chakra-ui/react';
 
-
 const ProductBox = () => {
-  // var data = useSelector((store)=>store)  
+  // var data = useSelector((store)=>store)
   // console.log(data);
-  let [state,setState]= useState([]);
-  let [page,setPage] = useState(1);
+  let [state, setState] = useState([]);
+  let [page, setPage] = useState(1);
   // console.log(state);
- 
-  const change = (id)=>{
-    setPage(id);
-  }
 
+  const change = (id) => {
+    setPage(id);
+  };
 
   let arr = [];
-for(let i = 1; i<=10; i++){
-  arr.push( <button onClick={()=>change(i)} key={i}>{i}</button>)
-}
-  
+  for (let i = 1; i <= 10; i++) {
+    arr.push(
+      <button onClick={() => change(i)} key={i}>
+        {i}
+      </button>
+    );
+  }
 
   let dispatch = useDispatch();
 
-useEffect(()=>{
+  useEffect(() => {
+    fetchhh();
+    //  dataUpdate();
+  }, []);
 
- fetchhh();
-//  dataUpdate();
-},[])
-
-
-const fetchhh = async()=>{
-  await fetch(`https://dineoutclone-foc1.onrender.com/products`)
-  .then((res)=>res.json())
-  .then((d)=>{
-    dispatch(ProductAction({d,setState}));
-    console.log(d)
-  setState(d);
-})
-}
-
-
-
+  const fetchhh = async () => {
+    await fetch(`https://dineoutclone-foc1.onrender.com/products`)
+      .then((res) => res.json())
+      .then((d) => {
+        dispatch(ProductAction({ d, setState }));
+        console.log(d);
+        setState(d);
+      });
+  };
 
   return (
-    <div className='a-productBox-container'>
+    <div className="a-productBox-container">
+      <nav className="a-productBox-nav">
+        <div>
+          <h2>
+            Best Restaurants Near Me in Delhi <span>({state.length})</span>
+          </h2>
+        </div>
 
-<nav className='a-productBox-nav'>
-  <div>
+        <div>
+          <span>Sort by</span>
+          <select>
+            <option value="Rating">Rating</option>
+            <option value="Popularity">Popularity</option>
+            <option value="Low to High">Price: Low to High</option>
+            <option value="High to Low">Price: High to Low</option>
+          </select>
+        </div>
+      </nav>
 
-<h2>Best Restaurants Near Me in Delhi <span>({state.length})</span></h2>
-  </div>
+      <div className="a-gridBox">
+        {state.map((e, i) => (
+          <Link to="" style={{ textDecoration: "none" }} key={i + 1}>
+            <DivProduct e={e} />{" "}
+          </Link>
+        ))}
+      </div>
 
-<div>
-  <span>Sort by</span>
-<select >
-  <option value="Rating">Rating</option>
-  <option value="Popularity">Popularity</option>
-  <option value="Low to High">Price: Low to High</option>
-  <option value="High to Low">Price: High to Low</option>
-</select>
-</div>
-</nav>
-
-
-<div className='a-gridBox'>
-{state.map((e,i)=> <Link to="" style={{textDecoration:"none"}}  key={i+1}><DivProduct e={e}/> </Link>)}
-</div>
-
-
-<div>
-{page}
-{arr}
-</div>
+      <div>
+        {page}
+        {arr}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ProductBox;
