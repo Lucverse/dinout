ff89a316860c1d63dfaa4b2534441b1fedc14235
spritualrahul <98818584+spritualrahul@users.noreply.github.com>
2023-01-06 13:42:31 +0530

    update

--- dineout/src/Components/LoginSignup/Login.jsx
+++ dineout/src/Components/LoginSignup/Login.jsx
@@ -1,3 +1,4 @@
+import { border } from "@chakra-ui/react"
 
 function Login({setLogin}) {
 
@@ -17,16 +18,16 @@ function Login({setLogin}) {
 
   return (
     <div style={overlay}>
-      <div style={{height:'20rem',width:'30rem', background:"white", position:"relative",borderRadius:'1rem',padding:"2rem"}}>
+      <div style={{height:'15rem',width:'25rem', background:"white", position:"relative",borderRadius:'3px',padding:"2rem"}}>
         <span onClick={(e) => setLogin(false)} style={{position:'absolute', right:"1rem",top:"1rem" }}>X</span>
         <h1 style={{color:'tomato', textAlign:'left',fontSize:'28px',marginBottom:"2rem"}}>Log In / Sign Up</h1>
         <input style={{display:'block',paddingBottom:"0.7rem", border:"none", outline:"none", borderBottom:'1px solid gray', width:'100%'}} type="text" placeholder="Enter Mobile Number or Email" />
         <button style={{display:'block', width:'100%', marginTop:"1.5rem",height:"2.5rem", background:"tomato", color:"white", border:"none"}}>Continue</button>
         <hr style={{position:"relative", top:"11.5%",zIndex:0 }} />
-        <p style={{background:"white", padding:"0 1rem", width:"fit", display:"inline-block", position:"relative"}}>Or Login via</p>
+        <p style={{background:"white", padding:"0 1rem", width:"fit", display:"inline-block", position:"relative",marginTop:"-10px"}}>Or Login via</p>
         <div style={{display:"flex",justifyContent:"center", gap:"1rem"}}>
-          <button style={{padding:"1rem  5rem"}}>Facebook</button>
-          <button style={{padding:"1rem  5rem"}}>Gmail</button>
+          <button style={{padding:"1rem  5rem",height:"40px",background:"#486FA4",border:"none",color:"white",alignItems:"center",justifyContent:"center",marginTop:"-10px",borderRadius:"3px"}}> <img style={{marginLeft:"-20px"}} src="https://im1.dineout.co.in/images/uploads/misc/2020/Jan/10/facebook-16.png" alt="logo" /> Facebook</button>
+          <button style={{padding:"1rem  5rem",height:"40px",color:"grey",background:"white",marginTop:"-10px"}}>Gmail</button>
 
         </div>
       </div>
